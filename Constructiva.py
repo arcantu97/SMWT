95948cef1345b84ca7a6c3156b257010ea1a9440
arcantu97 <42452569+arcantu97@users.noreply.github.com>
2018-11-17 01:11:42 -0600

    added local search

--- Constructiva.py
+++ Constructiva.py
@@ -1,19 +1,19 @@
 import numpy as np
 import itertools
-from itertools import combinations
+from itertools import permutations
+import collections
+import sys
 
 print("\n")
 archivo = str(input("\t\t\tIntroduce el nombre del archivo: "))
 fa = np.genfromtxt(archivo +".txt", delimiter=',', usecols=(0,1,2,3), dtype="int")[0:-1]
 N = np.genfromtxt(archivo+".txt", delimiter=',')[-1,0:1]
 N = int(N)
-print("\n")
 print("\t\t\tCantidad de procesos: ", N)
 
 # Convirtiendo array a lista y ordenando por pesos
 l = fa.tolist()
 l.sort(key=lambda x: x[3], reverse=True)
-t = l
 # Empezando heuristica constructiva
 TN = []
 C = []
@@ -36,10 +36,26 @@ for d in l:
 	t = d[5]
 	op = w*t 
 	ttp += op
-print("Orden de trabajos: ",TN)
-print("Tardanza Total Ponderada es: ", ttp)
+print("\n\n")
+print("\t\t\tConstructiva")
+print("\t\t\tTardanza Total Ponderada es: ", ttp)
+# ***********************************
 
+print("\n\n")
+print("\t\t\t Busqueda local")
+l2 = l
 ttp2 = 0
-cc = []
-for combinacion in itertools.permutations(l,):
-	print(combinacion)
+m = list(permutations(l2))
+for i in m:
+	for x in i:
+		w = x[3]#elemento 3 de la permutacion x
+		t = x[5]#elemento 5 de la permutacion x
+		op = w*t
+		ttp2 += op
+		if ttp2 < ttp:
+			if ttp2 == 0:
+				break
+			if ttp2 > 0:
+				print("\t\t\tTardanza Total Ponderada es: ", ttp2)
+				break
+
